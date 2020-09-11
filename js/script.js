7f3ef3601efe29fd9949b65d36155b7d43f24aad
JoseDanielMX <jduvmx@gmail.com>
2020-09-11 15:56:35 -0500

    Created a list of answers and a function to get one of these answers randomly

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	var magic8Ball = {};
+	magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so...", "Of course!", "Indubitably", "In your dreams."];
+
+	// Define the method
+	magic8Ball.askQuestion = function(question) {
+
+		 // Create a random number
+		 var randomNumber = Math.random();
+
+		 // Make the random number between 0 and the number of items in your array
+		 var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+
+		 // Round down the number to be a whole number
+		 var randomIndex = Math.floor(randomNumberForListOfAnswers);
+
+		 // Use that number to index a random number from the answers array
+		 var answer = this.listOfAnswers[randomIndex];
+
+		 console.log(question);
+		 console.log(answer);
+	};
+});
